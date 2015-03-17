892daf0eaf94b61d32f2e591400cd27533f06d3d
Tomas Petricek <tomas@tomasp.net>
2015-03-17 16:38:40 +0100

    [LPLondon] Adding the talk and images

---
+++ 2015/literate-programming-v2/slides/talk.fsx
@@ -0,0 +1,382 @@
+﻿(**
+- title : Literate programming with F#
+- description : In this presentation, we look at the F# tools for literate 
+  programming, including the F# Formatting library (which parses literate F# and 
+  Markdown), ProjectScaffold (a template for projects that lets you write literate 
+  documentation) and FsReveal (a tool for creating presentation using F#). You'll 
+  learn useful things about documenting your (not just F#) code and about writing 
+  understandable code and data analyses.
+- author : Tomas Petricek
+- theme : Night
+- transition : none
+
+*******************************************************************************
+
+## Literate programming with F#
+
+<br /><br />
+<img src="images/literate.png" style="width:200px;border-style:none;background:transparent;" />
+<br /><br /><br /><br />
+
+**Tomas Petricek**, F# Works  
+[@tomaspetricek](http://twitter.com/tomaspetricek) 
+| [http://tomasp.net](http://tomasp.net) 
+| [http://fsharpworks.com](http://fsharpworks.com)
+
+*******************************************************************************
+
+## What is literate programming?
+
+<br /><br /><br />
+<img src="images/literate.png" style="width:200px;border-style:none;background:transparent;" />
+
+-------------------------------------------------------------------------------
+
+## What is literate programming?
+
+<div class="fragment">
+<br />
+
+    [lang=csharp]
+    public class Person {
+      /// <summary>
+      /// Gets or sets the name of the person
+      /// </summary>
+      public string Name { get; set; }
+
+      /// <summary>
+      /// Gets or sets the age of the person
+      /// </summary>
+      public string Age { get; set; }
+    }
+
+<br />
+
+This is *not* literate programming!
+
+</div>
+
+-------------------------------------------------------------------------------
+
+## What is literate programming?
+
+<br />
+
+![Donald Knuth](images/knuth.jpg)
+
+Professor Donald Knuth is not happy :-(
+
+-------------------------------------------------------------------------------
+
+<img src="images/book.jpg" style="width:400px" />
+
+-------------------------------------------------------------------------------
+
+## What is literate programming?
+
+<br />
+
+> Let us change our traditional attitude to the construction of programs: Instead 
+> of imagining that our main task is to instruct a __computer__ what to do, let us 
+> concentrate rather on explaining to __human beings__ what we want a computer to do.
+
+-------------------------------------------------------------------------------
+
+## The `WEB` system
+
+> `WEB` is a combination of two other languages
+>
+>  1. a document formatting language and 
+>  2. a programming language. 
+>
+> I chose the name `WEB` partly because it was one of the few three-letter 
+> words of English that hadn’t already been applied to computers. 
+
+-------------------------------------------------------------------------------
+
+## The `WEB` system
+
+    [lang=text]
+    @* Printing primes: An example of \WEB. 
+    \[The program text below specifies the ``expanded meaning'';
+    notice that it involves the top-level descriptions of three 
+    other sections. When those top-level descriptions are replaced 
+    by their expanded meanings, a syntactically correct \PASCAL\ 
+    program will be obtained.\] 
+    
+    @<Program to print...@>= 
+      program print_primes(output); 
+      const @!m=1000; 
+      @<Other constants of the program@>@; 
+      var @<Variables of the program@>@; 
+      begin @<Print the first |m| prime numbers@>; 
+      end.
+
+*******************************************************************************
+
+## Literate programming with F#
+
+<br /><br /><br />
+<img src="images/literate.png" style="width:200px;border-style:none;background:transparent;" />
+
+-------------------------------------------------------------------------------
+*)
+(*** hide ***)
+#load "../tomas/svg.fsx"
+open FsReveal.SmartArt
+open System.Drawing
+
+module Diagrams = 
+  let mks = 
+    Split(Vertical, None, [
+      Align(BottomMiddle, tx "F# + Markdown"); 
+      Align(TopMiddle, tx "Source")
+    ])
+
+  let diag = 
+    (mks, color "#1F5B56", Color.White) ==>
+      [ (tx "F# Code", color "#325E6B", Color.White) ==>
+          [ nd(tx "F# Interactive", color "#437E8E", Color.White)
+            nd(tx "F# Project", color "#437E8E", Color.White) ]
+        (tx "Markdown", color "#31683F", Color.White) ==>
+          [ nd(tx "FsReveal", color "#438E49", Color.White)
+            nd(tx "PDF Reports", color "#438E49", Color.White) 
+            nd(tx "Documentation", color "#438E49", Color.White) ] ]
+  let d1 = WithSize(800, 400, Draw(diag))
+(**
+## Literate programming with F#
+
+<br /><br />
+
+*)
+(*** include-value: Diagrams.d1 ***)
+(**
+
+-------------------------------------------------------------------------------
+
+## Literate slides with FsReveal
+
+<br />
+
+No F# demo is complete without the `|>` operator...
+*)
+(*** define-output:hello ***)
+[ "Hello"; " "; "Krakow"; "!" ]
+|> String.concat ""
+|> printfn "%s"
+(**
+<br />
+
+FsReveal embeds the output automatically:
+*)
+(*** include-output:hello ***)
+(**
+
+-------------------------------------------------------------------------------
+
+## Literate slides with FsReveal
+
+<br />
+*)
+(*** hide ***)
+#load @"..\packages\FsLab\FsLab.fsx"
+open FSharp.Charting
+open System.Drawing
+open FSharp.Data
+(**
+Get School Enrollment data from WorldBank
+*)
+let wb = WorldBankData.GetDataContext()
+let cz = wb.Countries.``Czech Republic``.Indicators
+let po = wb.Countries.Poland.Indicators
+let eu = wb.Countries.``European Union``.Indicators
+(**
+Compare Czech Republic and EU stats
+*)
+(*** define-output:chart ***)
+[ Chart.Line
+    ( cz.``School enrollment, tertiary (% gross)``, 
+      Color = Color.HotPink)
+  Chart.Line
+    ( po.``School enrollment, tertiary (% gross)``, 
+      Color = Color.White)
+  Chart.Line
+    ( eu.``School enrollment, tertiary (% gross)``, 
+      Color = Color.CornflowerBlue) ]
+|> Chart.Combine
+(**
+-------------------------------------------------------------------------------
+
+## Literate slides with FsReveal
+
+<br />
+
+FsReveal embeds the chart for us!
+*)
+(*** include-it:chart ***)
+(**
+
+*******************************************************************************
+
+## Looking under the cover...
+
+<br /><br /><br />
+<img src="images/literate.png" style="width:200px;border-style:none;background:transparent;" />
+
+-------------------------------------------------------------------------------
+
+## Looking under the cover...
+
+Enabled by the great F# community!
+
+<br />
+<div class="fragment">
+
+ - **[F# Formatting](http://tpetricek.github.io/FSharp.Formatting/)** to process F# scripts & Markdown
+ - **[F# Compiler Service](http://fsharp.github.io/FSharp.Compiler.Service/)** for tool tips & evaluation
+ - **[FsReveal](http://fsprojects.github.io/FsReveal/)** to generate reveal.js slides
+ - **[FsLab](http://www.fslab.org)** data-science libraries for charts
+ - **[FAKE](http://fsprojects.github.io/FAKE) & [Suave](http://suave.io/)** to put things together & host
+
+</div>
+
+-------------------------------------------------------------------------------
+
+## Literate F#: Using F# Scripts
+
+<br />
+
+Write an F# script with special comments
+
+<br />
+
+    (** Use _Markdown_ in comments. *)
+    (*** define-output:hello ***)
+    printfn "Hello world!"
+    (** write code as usual... *)
+    (*** include-output:hello ***)
+
+-------------------------------------------------------------------------------
+
+## Literate F#: Markdown mode
+
+<br />
+
+Write Markdown document with F# code snippets
+
+<br />
+
+    [lang=text]
+    Write standard _Markdown_ document 
+
+        [lang=fsharp]
+        printfn "Hello world!"
+    
+    With embedded F# snippets
+
+-------------------------------------------------------------------------------
+*)
+(*** hide ***)
+module D2 =
+  let MakeBox(backColor, content) =
+    [ Fill(HtmlColor backColor, RoundedRectangle(10G, 10G)); 
+      Fill(HtmlColor "#FFFFFF", Graphics.Text content)]
+    |> Combine
+    |> WithMargin (10G, 10G, 10G, 10G)
+  let svg = 
+    Split(Horizontal, None, [
+      MakeBox("#1F5B56", "Diagrams")
+      MakeBox("#325E6B", "..are..")
+      MakeBox("#31683F", "boring :-(")
+    ])
+  let d2 = WithSize(800, 300, Align(Center, svg))
+(**
+## Literate F#: Diagrams
+
+<br />
+
+We can embed results and even charts. How about...
+
+<div class="fragment">
+*)
+(*** include-value: D2.d2 ***)
+(**
+</div>
+
+-------------------------------------------------------------------------------
+
+## Literate F#: Diagrams
+
+Domain specific language for building diagrams!
+*)
+open FsReveal.SmartArt
+
+let MakeBox(backColor, content) =
+  [ Fill(HtmlColor backColor, RoundedRectangle(10G, 10G)); 
+    Fill(HtmlColor "#FFFFFF", Text content)]
+  |> Combine |> WithMargin (10G, 10G, 10G, 10G)
+
+let svg = Split(Horizontal, None, [
+    MakeBox("#1F5B56", "Diagrams")
+    MakeBox("#325E6B", "..are..")
+    MakeBox("#31683F", "fun!")
+  ])
+(**
+
+*******************************************************************************
+
+## Literate tools for F#
+
+<br /><br /><br />
+<img src="images/literate.png" style="width:200px;border-style:none;background:transparent;" />
+
+-------------------------------------------------------------------------------
+
+## FsLab Journal
+
+<a href="images/FsLab_Tutorial1.html" target="_blank">
+<img src="images/journal.png" style="border-style:none;width:450px" />
+</a>
+
+Download the [FsLab Journal Template](https://visualstudiogallery.msdn.microsoft.com/45373b36-2a4c-4b6a-b427-93c7a8effddb)!
+
+-------------------------------------------------------------------------------
+
+## ProjectScaffold docs
+
+<a href="http://fsharp.github.io/FSharp.Data/library/WorldBank.html" target="_blank">
+<img src="images/scaffold.png" style="border-style:none;width:550px" />
+</a>
+
+See [ProjectScaffold](fsprojects.github.io/ProjectScaffold/) on GitHub for more!
+
+*******************************************************************************
+
+## Conclusions
+
+<br /><br /><br />
+<img src="images/literate.png" style="width:200px;border-style:none;background:transparent;" />
+
+-------------------------------------------------------------------------------
+
+### Literate scripting
+
+Journal or notebooks for data science  
+Walkthrough tutorials in documentation
+
+<br />
+
+### Literate software engineering
+
+This is still an interesting problem!
+
+<br /><br />
+
+**Tomas Petricek**, F# Works  
+[@tomaspetricek](http://twitter.com/tomaspetricek) 
+| [http://tomasp.net](http://tomasp.net) 
+| [http://fsharpworks.com](http://fsharpworks.com)
+
+Thanks to Karlkim Suwanmongkol ([@kimsk](https://twitter.com/kimsk)) for creating FsReveal!
+
+*)
---
+++ 2015/literate-programming-v2/tomas/svg.fsx
@@ -0,0 +1,366 @@
+﻿module FsReveal.SmartArt
+
+#r "System.Xml.Linq.dll"
+open System
+open System.Globalization
+open System.Drawing
+open System.Xml.Linq
+
+// --------------------------------------------------------------------------------------
+// Helpers
+// --------------------------------------------------------------------------------------
+
+let (!) s = XName.Get(s)
+
+let writeHtml (html:string) =
+  let template = """
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<body>
+  <svg id="root" height="100" width="100">[SVG]</svg>
+</body>
+</html>"""
+  let res = template.Replace("[SVG]", html)
+  System.IO.File.WriteAllText("C:\\temp\\diagrams.html", res)
+
+// --------------------------------------------------------------------------------------
+// SVG domain model
+// --------------------------------------------------------------------------------------
+
+type Size = 
+  | Relative of float 
+  | Absolute of float
+
+type Margin = 
+  { Top : Size
+    Right : Size
+    Bottom : Size
+    Left : Size }
+
+type SplitKind = 
+  | Vertical 
+  | Horizontal
+
+type LineCap = Butt | Square | Round
+type LineJoin = Miter | Bevel | Round
+
+type Fill = 
+  | Color of Color
+  //| Gradient
+  //| Pattern
+
+type Stroke = 
+  { Color : Color 
+    Width : float
+    LineCap : LineCap
+    LineJoin : LineJoin
+    DashArray : int list }
+
+type Text = string
+
+type TextAlignment = 
+  | TopLeft 
+  | TopMiddle
+  | TopRight
+  | CenterLeft
+  | Center
+  | CenterRight
+  | BottomLeft
+  | BottomMiddle
+  | BottomRight
+
+type Graphics =
+  | Empty
+  | Rectangle
+  | RoundedRectangle of Size * Size
+  | Ellipse
+  | Text of Text
+
+  | Margin of Margin * Graphics
+
+  | Combine of Graphics list
+  | Split of SplitKind * Size list option * Graphics list
+
+  | Align of TextAlignment * Graphics
+  | Fill of Fill * Graphics
+  | Stroke of Stroke * Graphics
+
+type SizedGraphics =
+  { Graphics : Graphics 
+    Width : int
+    Height : int }
+
+// --------------------------------------------------------------------------------------
+// SVG rendering
+// --------------------------------------------------------------------------------------
+
+type Rectangle =
+  { Left : float
+    Top : float
+    Width : float
+    Height : float }
+
+type RenderContext =
+  { BoundingBox : Rectangle 
+    Alignment : TextAlignment
+    Stroke : Stroke 
+    Fill : Fill }
+
+let size total = function
+  | Absolute size -> size
+  | Relative size -> total * size / 100.0
+
+let applyMargin (margin:Margin) (rect:Rectangle) = 
+  let l, t = size rect.Width margin.Left, size rect.Height margin.Top
+  let r, b = size rect.Width margin.Right, size rect.Height margin.Bottom
+  { Rectangle.Left = rect.Left + l
+    Top = rect.Top + t
+    Width = rect.Width - l - r
+    Height = rect.Height - t - b }
+
+let formatColor (color:Color) = 
+  (float color.A) / 255.0, sprintf "rgb(%d,%d,%d)" color.R color.G color.B
+
+let formatFill (fill:Fill) = 
+  match fill with 
+  | Fill.Color color ->
+      let opacity, color = formatColor color
+      [ XAttribute(!"fill", color)
+        XAttribute(!"fill-opacity", opacity) ]
+  
+let formatStroke (stroke:Stroke) = 
+  if stroke.Width > 0.0 then
+    let opacity, color = formatColor stroke.Color
+    [ XAttribute(!"stroke", color)
+      XAttribute(!"stroke-opacity", opacity)
+      XAttribute(!"stroke-linecap", (sprintf "%A" stroke.LineCap).ToLower())
+      XAttribute(!"stroke-linejoin", (sprintf "%A" stroke.LineJoin).ToLower())
+      XAttribute(!"stroke-width", stroke.Width) ] @
+    match stroke.DashArray with
+    | [] -> []
+    | nums -> [XAttribute(!"stroke-dasharray", nums |> Seq.map (sprintf "%d") |> String.concat ",") ]
+  else []
+
+let formatRectangle (rx, ry) (rect:Rectangle) = 
+  [ yield XAttribute(!"width", rect.Width)
+    yield XAttribute(!"height", rect.Height)
+    yield XAttribute(!"x", rect.Left)
+    yield XAttribute(!"y", rect.Top) 
+    if rx <> Relative 0.0 && rx <> Absolute 0.0 then 
+      yield XAttribute(!"rx", size rect.Width rx)
+    if ry <> Relative 0.0 && ry <> Absolute 0.0 then
+      yield XAttribute(!"ry", size rect.Height ry) ]
+
+let formatEllipse (rect:Rectangle) =
+  [ XAttribute(!"rx", rect.Width/2.0)
+    XAttribute(!"ry", rect.Height/2.0)
+    XAttribute(!"cx", rect.Left + rect.Width/2.0)
+    XAttribute(!"cy", rect.Top + rect.Height/2.0) ]
+
+let formatTextAlignment (rect:Rectangle) align =
+  [ match align with 
+    | TopLeft | TopMiddle | TopRight ->
+        yield XAttribute(!"y", rect.Top)
+        yield XAttribute(!"dy", "1em")
+    | CenterLeft | Center | CenterRight ->
+        yield XAttribute(!"y", rect.Top + rect.Height/2.0)
+        yield XAttribute(!"dy", "0.30em")
+    | BottomLeft | BottomMiddle | BottomRight ->
+        yield XAttribute(!"y", rect.Top + rect.Height) 
+    match align with
+    | TopLeft | CenterLeft | BottomLeft ->
+        yield XAttribute(!"x", rect.Left)
+        yield XAttribute(!"text-anchor", "start") 
+    | TopMiddle | Center | BottomMiddle ->
+        yield XAttribute(!"x", rect.Left + rect.Width/2.0)
+        yield XAttribute(!"text-anchor", "middle") 
+    | TopRight | CenterRight | BottomRight ->
+        yield XAttribute(!"x", rect.Left + rect.Width)
+        yield XAttribute(!"text-anchor", "end") ]
+
+let formatText ctx (t:string) = 
+  [ for attr in formatTextAlignment ctx.BoundingBox ctx.Alignment do
+      yield box attr
+    yield box t ]
+
+let rec formatGraphics ctx = function
+  | Empty -> []
+  | RoundedRectangle(rx, ry) -> 
+      [ XElement(!"rect", formatRectangle (rx, ry) ctx.BoundingBox, formatStroke ctx.Stroke, formatFill ctx.Fill) ]
+  | Rectangle -> 
+      [ XElement(!"rect", formatRectangle (Absolute 0.0, Absolute 0.0) ctx.BoundingBox, formatStroke ctx.Stroke, formatFill ctx.Fill) ]
+  | Ellipse -> 
+      [ XElement(!"ellipse", formatEllipse ctx.BoundingBox, formatStroke ctx.Stroke, formatFill ctx.Fill) ]
+  | Text t ->
+      [ XElement(!"text", formatText ctx t, formatStroke ctx.Stroke, formatFill ctx.Fill) ]
+
+  | Align(align, graphics) -> formatGraphics { ctx with Alignment = align } graphics
+  | Fill(fill, graphics) -> formatGraphics { ctx with Fill = fill } graphics
+  | Stroke(stroke, graphics) -> formatGraphics { ctx with Stroke = stroke } graphics
+
+  | Margin(margin, graphics) ->
+      let bbox = applyMargin margin ctx.BoundingBox
+      let ctx = { ctx with BoundingBox = bbox }
+      formatGraphics ctx graphics
+
+  | Combine(graphics) ->
+      graphics |> List.collect (formatGraphics ctx)
+
+  | Split(Horizontal, sizes, graphics) ->
+      let count = float graphics.Length
+      let sizes =
+        match sizes with
+        | None -> [ for _ in graphics -> Absolute (ctx.BoundingBox.Width / count) ]
+        | Some s -> s
+      let left = ref ctx.BoundingBox.Left
+      Seq.zip graphics sizes
+      |> Seq.mapi (fun i (gr, subSize) ->
+        let subSize = size ctx.BoundingBox.Width subSize
+        let bbox = { ctx.BoundingBox with Width = subSize }
+        let ctx = { ctx with BoundingBox = { bbox with Left = left.Value } }
+        left := left.Value + subSize
+        formatGraphics ctx gr )
+      |> Seq.concat |> List.ofSeq 
+
+  | Split(Vertical, sizes, graphics) ->
+      let count = float graphics.Length
+      let sizes =
+        match sizes with
+        | None -> [ for _ in graphics -> Absolute (ctx.BoundingBox.Height / count) ]
+        | Some s -> s
+      let top = ref ctx.BoundingBox.Top
+      Seq.zip graphics sizes
+      |> Seq.mapi (fun i (gr, subSize) ->
+        let subSize = size ctx.BoundingBox.Height subSize
+        let bbox = { ctx.BoundingBox with Height = subSize }
+        let ctx = { ctx with BoundingBox = { bbox with Top = top.Value } }
+        top := top.Value + subSize
+        formatGraphics ctx gr )
+      |> Seq.concat |> List.ofSeq 
+
+module Palette =
+  let Vega10 = seq {
+      while true do
+        for c in [ "ff1f77b4"; "ffff7f0e"; "ff2ca02c"; "ffd62728"; "ff9467bd"; "ff8c564b"; "ffe377c2"; "ff7f7f7f"; "ffbcbd22"; "ff17becf" ] do
+          yield Color.FromArgb(Int32.Parse(c, NumberStyles.HexNumber)) }
+  let Vega20 = seq {
+      while true do
+        for c in [ "ff1f77b4"; "ffaec7e8"; "ffff7f0e"; "ffffbb78"; "ff2ca02c"; "ff98df8a"; "ffd62728"; "ffff9896"; "ff9467bd"; "ffc5b0d5"; "ff8c564b"; "ffc49c94"; "ffe377c2"; "fff7b6d2"; "ff7f7f7f"; "ffc7c7c7"; "ffbcbd22"; "ffdbdb8d"; "ff17becf"; "ff9edae5" ] do
+          yield Color.FromArgb(Int32.Parse(c, NumberStyles.HexNumber)) }
+
+
+module Chart = 
+  let Bar(yvalues) = 
+    let lo, hi = Seq.min yvalues, Seq.max yvalues
+    let margin = 
+      { Margin.Top = Absolute 0.0; Left = Absolute 0.0; Bottom = Absolute 0.0; Right = Absolute 1.0 }
+    let bars = 
+      [ for yval, clr in Seq.zip yvalues Palette.Vega10 ->
+          let margin = { margin with Top = Relative (100.0 - yval/hi*100.0) }
+          let body = Combine[Rectangle; Align(TopMiddle, Text(string yval)) ]
+          Fill(Fill.Color clr, Margin(margin, body)) ]
+    Split(Horizontal, None, bars)
+
+let rect = { Rectangle.Left = 0.0; Top = 0.0; Width = 100.0; Height = 100.0 }
+let stroke = { Stroke.Color = Color.MidnightBlue; LineCap = LineCap.Round; LineJoin = LineJoin.Round; Width = 0.0; DashArray = [] }
+let fill = Fill.Color Color.LightSteelBlue
+let ctx = { BoundingBox = rect; Stroke = stroke; Fill = fill; Alignment = TopRight }
+
+
+// let m = { Margin.Left = Relative 10.0; Right = Relative 10.0; Top = Relative 20.0; Bottom = Relative 20.0 }
+// let svg = Split(Horizontal, [Rectangle; Margin(m, Rectangle); Ellipse])
+
+//let svg = Chart.Bar [ 10.0; 40.0; 15.0; 60.0; 55.0; 25.0 ]
+//let svg = Split(Vertical, [ Combine [ Rectangle; Text("Hello world") ]; Ellipse ])
+
+let data = [ 60.0; 50.0; 30.0; 20.0; 10.0; 5.0; ]
+let half = (Seq.sum data) / 2.0
+let prefixes = [ for i in 1 .. data.Length-2 -> abs((data |> Seq.take i |> Seq.sum)-half)]
+let count = prefixes |> Seq.mapi (fun i v -> i, v) |> Seq.minBy snd
+
+type Graphics with
+  member x.Render(w, h) = 
+    let rect = { Rectangle.Left = 0.0; Top = 0.0; Width = float w; Height = float h }
+    let stroke = { Stroke.Color = Color.MidnightBlue; LineCap = LineCap.Round; LineJoin = LineJoin.Round; Width = 0.0; DashArray = [] }
+    let fill = Fill.Color Color.LightSteelBlue
+    let ctx = { BoundingBox = rect; Stroke = stroke; Fill = fill; Alignment = TopRight }
+    let html = 
+      x |> formatGraphics ctx
+        |> Seq.map (fun e -> e.ToString()) |> String.concat ""
+    sprintf """<svg id="root" height="%d" width="%d">%s</svg>""" h w html
+  member x.ToHtml() = x.Render(100, 100)
+
+type SizedGraphics with
+  member x.ToHtml() = x.Graphics.Render(x.Width, x.Height)
+    
+let WithSize(w, h, g) = { Graphics = g; Width = w; Height = h }
+// svg |> formatGraphics ctx |> writeHtml
+
+type Tree = Tree of (Graphics * Color * Color) * Tree list
+
+let smallMarg = { Margin.Top = Relative 5.0; Left = Relative 5.0; Bottom = Relative 5.0; Right = Relative 5.0 }
+let largeMarg = { Margin.Top = Relative 30.0; Left = Relative 5.0; Bottom = Relative 30.0; Right = Relative 5.0 }
+
+let rec depth = function
+  | Tree(_, []) -> 1
+  | Tree(_, ch) -> 1 + (List.map depth ch |> List.max)
+
+let rec countLeafs = function
+  | Tree(_, []) -> 1
+  | Tree(_, ch) -> List.sumBy countLeafs ch
+
+let rec drawTree ((Tree((text, backColor, textColor), children)) as t) =
+  let box = Combine [Fill(Color backColor, RoundedRectangle(Absolute 10.0,Absolute 10.0)); Fill(Color textColor,text)]
+  let marg = if children.Length <= 1 then smallMarg else largeMarg
+  if children = [] then
+    Margin(smallMarg, box)
+  else
+    let d = float (depth t)
+    let childCounts = List.map (countLeafs >> float) children
+    let childTotal = childCounts |> List.sum
+    let splitSizes = childCounts |> List.map (fun c -> Relative(100.0/childTotal*c))
+    let nested = Split(Vertical, Some splitSizes, List.map drawTree children)
+    let split = Some [Relative(100.0/d); Relative(100.0-100.0/d)]
+    Split(Horizontal, split, [Margin(marg, box); nested])
+
+let (==>) root children = Tree(root, children)
+let nd root = Tree(root, [])
+let tx s = Text(s)
+
+let Draw(diag) = Align(Center, drawTree diag)
+let color ht = ColorTranslator.FromHtml(ht)
+
+(*
+let diag = 
+  ("Source", color "#1F5B56", Color.White) ==>
+    [ ("Tex", color "#325E6B", Color.White) ==>
+        [ nd("Exe", color "#437E8E", Color.White) ]
+      ("Pas", color "#31683F", Color.White) ==>
+        [ nd("Pdf", color "#438E49", Color.White) 
+          nd("Html", color "#438E49", Color.White) ] ]
+let out = WithSize(400, 300, Draw(diag))
+
+//let marg = { Margin.Top = Relative 5.0; Left = Relative 5.0; Bottom = Relative 5.0; Right = Relative 5.0 }
+//Align(Center, Split(Horizontal, [Margin(marg, Combine [RoundedRectangle(Absolute 10.0,Absolute 10.0); Text "hi"]); Margin(marg, Combine [Rectangle; Text "hi"])]))
+*)
+
+module NumericLiteralG =
+  let FromInt32 n = Absolute(float n)
+
+let HtmlColor c = Color(ColorTranslator.FromHtml(c))
+let WithMargin (t,r,b,l) c = Margin({Top=t; Right=r;Bottom=b;Left=l}, c)
+(*
+let MakeBox(backColor, content) =
+  [ Fill(HtmlColor backColor, RoundedRectangle(10G, 10G)); 
+    Fill(HtmlColor "#FFFFFF", Text content)]
+  |> Combine
+  |> WithMargin (10G, 10G, 10G, 10G)
+let svg = 
+  Split(Horizontal, None, [
+    MakeBox("#1F5B56", "Diagrams")
+    MakeBox("#325E6B", "..are..")
+    MakeBox("#31683F", "fun!")
+  ])
+let out = WithSize(700, 450, Align(Center, svg))
+out.ToHtml() |> writeHtml
+*)
