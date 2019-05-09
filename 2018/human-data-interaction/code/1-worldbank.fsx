0358e513e9172469a3785c1bcd73bbbfa9450e2d
Tomas Petricek <tomas@tomasp.net>
2019-05-09 09:39:08 +0100

    Update JSON edit demo, paket

--- 2018/human-data-interaction/code/1-worldbank.fsx
+++ 2018/human-data-interaction/code/1-worldbank.fsx
@@ -7,9 +7,3 @@ let wb = WorldBankData.GetDataContext()
 
 // GINI index in the US and UK over time
 // GINI index in 2010 in all countries (filter nan)
-
-[ for c in wb.Countries do
-    let v = c.Indicators.``GINI index (World Bank estimate)``.[2010]
-    if not (System.Double.IsNaN v) then
-      yield c.Name, v ]
-|> Chart.Geo
--- 2018/human-data-interaction/code/2-rss.fsx
+++ 2018/human-data-interaction/code/2-rss.fsx
@@ -5,10 +5,3 @@ open FSharp.Data
 // http://feeds.bbci.co.uk/news/politics/rss.xml
 // http://feeds.bbci.co.uk/news/world/rss.xml
 // Format date using "D"
-
-type Rss = XmlProvider<"http://feeds.bbci.co.uk/news/world/rss.xml">
-let rss = Rss.Load("http://feeds.bbci.co.uk/news/politics/rss.xml")
-
-for it in rss.Channel.Items do
-  printfn " * %s (%s)" it.Title (it.PubDate.ToString "D")
-
